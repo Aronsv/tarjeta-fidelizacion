a6bebfb271a611cb2e0c1e7f32a12efcf6ff6cc3
Aronsv <49215005+Aronsv@users.noreply.github.com>
2026-03-18 15:10:11 -0500

    Update script.js
    conexion al firebase

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-function registrar() {
+// IMPORTAR FIREBASE
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// CONFIG FIREBASE
+const firebaseConfig = {
+  apiKey: "AIzaSyCcs-eMp-GQCIXO1zDiiadeLhbETcqdtn8",
+  authDomain: "tarjeta-fidelizacion-563aa.firebaseapp.com",
+  projectId: "tarjeta-fidelizacion-563aa",
+  storageBucket: "tarjeta-fidelizacion-563aa.firebasestorage.app",
+  messagingSenderId: "256189396604",
+  appId: "1:256189396604:web:115c60825caf2def193bf9"
+};
+
+// INICIALIZAR
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// FUNCIÓN REGISTRAR
+window.registrar = async function () {
   const input = document.getElementById("usuario");
   const mensaje = document.getElementById("mensaje");
   const usuario = input.value.trim();
 
-  // Validar vacío
-  if (usuario === "") {
-    mensaje.innerText = "⚠️ Ingresa tu número de celular";
+  if (usuario === "" || usuario.length !== 9) {
+    mensaje.innerText = "⚠️ Número inválido";
     mensaje.style.color = "red";
     return;
   }
 
-  // Validar solo números
-  if (!/^\d+$/.test(usuario)) {
-    mensaje.innerText = "⚠️ Solo se permiten números";
+  try {
+    await addDoc(collection(db, "visitas"), {
+      celular: usuario,
+      fecha: new Date()
+    });
+
+    mensaje.innerText = "✅ Registro guardado";
+    mensaje.style.color = "green";
+    input.value = "";
+
+  } catch (error) {
+    mensaje.innerText = "❌ Error al guardar";
     mensaje.style.color = "red";
-    return;
+    console.error(error);
   }
-
-  // Validar 9 dígitos
-  if (usuario.length !== 9) {
-    mensaje.innerText = "⚠️ El número debe tener 9 dígitos";
-    mensaje.style.color = "red";
-    return;
-  }
-
-  // Si todo está bien
-  mensaje.innerText = "✅ Registro enviado correctamente";
-  mensaje.style.color = "green";
-
-  input.value = "";
-}
+};
